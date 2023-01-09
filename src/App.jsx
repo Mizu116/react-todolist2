c0bc938bfba5ff4010d3c6c8745d0c5035b316c0
Mizu <mija.1521@gmail.com>
2023-01-09 22:32:37 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import "./style.css";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [editModeIsActive, setEditModeIsActive] = useState(false);
+  const [editTodo, setEditTodo] = useState("");
+  const [editIndex, setEditIndex] = useState();
+  const [filter, setFilter] = useState("all");
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          text: todo.trim(),
+          id: todos.length === 0 ? 1 : todos[todos.length - 1].id + 1,
+          status: "inactive",
+        },
+      ]);
+      setTodo("");
+    }
+  }
+
+  function handleEditSubmit(e) {
+    e.preventDefault();
+    if (editTodo === "") {
+      handleDelete(editIndex);
+    } else {
+      setTodos(
+        todos.map((entry, index) => {
+          if (index === editIndex) {
+            return { text: editTodo, id: entry.id, status: entry.status };
+          } else return entry;
+        })
+      );
+      setEditModeIsActive(false);
+    }
+  }
+
+  function handleInputChange(e) {
+    setTodo(e.target.value);
+  }
+
+  function handleEditTodo(e) {
+    setEditTodo(e.target.value);
+  }
+
+  function handleEditMode(index) {
+    console.log(todos);
+    setEditTodo(todos[index].text);
+    setEditIndex(index);
+    setEditModeIsActive(true);
+  }
+
+  function handleDelete(index) {
+    setTodos(
+      todos.filter((_todo, indexTodos) => {
+        return indexTodos !== index;
+      })
+    );
+    setEditModeIsActive(false);
+  }
+
+  function handleOptionSelect(e, idEvent) {
+    e.preventDefault();
+    setTodos(
+      todos.map((entry) => {
+        if (entry.id === idEvent) {
+          return { text: entry.text, id: entry.id, status: e.target.value };
+        } else return entry;
+      })
+    );
+  }
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      {editModeIsActive === false && (
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="addtodotext"
+            name="todo"
+            type="text"
+            placeholder="Todoを入力"
+            value={todo}
+            onChange={handleInputChange}
+          />
+          <input
+            name="submit"
+            type="submit"
+            value="追加"
+            className="button"
+          />
+          <select className='selector' value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="all">全て</option>
+            <option value="inactive">着手予定</option>
+            <option value="incomplete">着手</option>
+            <option value="complete">完了</option>
+          </select>
+        </form>
+      )}
+      {editModeIsActive && (
+        <form onSubmit={handleEditSubmit}>
+          <input
+            name="editTodo"
+            type="text"
+            value={editTodo}
+            onChange={handleEditTodo}
+          />
+          <input
+            name="submit"
+            type="submit"
+            value="完了"
+            className="Addbutton"
+          />{" "}
+          <select className='selector' value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="all">全て</option>
+            <option value="inactive">着手予定</option>
+            <option value="incomplete">着手</option>
+            <option value="complete">完了</option>
+          </select>
+        </form>
+      )}
+      <ul className="todo-list">
+        {todos.map(
+          (todo, index) =>
+            (todo.status === filter || filter === "all") && (
+              <li key={todo.id}>
+                <span className="todo-text">{todo.text}</span>
+                <select
+                  className='selector'
+                  value={todo.status}
+                  onChange={(e) => handleOptionSelect(e, todo.id)}
+                >
+                  <option value="inactive">着手予定</option>
+                  <option value="incomplete">着手</option>
+                  <option value="complete">完了</option>
+                </select>
+                <button className="button" onClick={() => handleEditMode(index)}>編集</button>
+                <button className="button" onClick={() => handleDelete(index)}>削除</button>
+              </li>
+            )
+        )}
+      </ul>
+    </div>
+  );
+}
